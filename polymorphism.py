e5ef04e42969130c36f91a59aa605412ed293799
Vivek Wisdom <ervivekwisdom@gmail.com>
2017-08-07 16:25:15 +0530

    concepts: Added code snippet for Polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,66 @@
+""" Using polymorphism and complex OOP consepts in Python """
+#!/usr/bin/python3
+
+class AnimalActions(object):
+    def quack(self):
+        """ In the forest method """
+        return self.strings['quack']
+    def feathers(self):
+        """ In the forest method """
+        return self.strings['feathers']
+    def bark(self):
+        """ In the forest method """
+        return self.strings['bark']
+    def fur(self):
+        """ In the forest method """
+        return self.strings['fur']
+
+class Duck(AnimalActions):
+    strings = dict(
+        quack = "Quaaaaak!",
+        feathers = "The duck has gray and white feathers.",
+        bark = "The duck cannot bark.",
+        fur = "The duck has no fur."
+    )
+ 
+class Person(AnimalActions):
+    strings = dict(
+        quack = "The person imitates a duck.",
+        feathers = "The person takes a feather from the ground and shows it.",
+        bark = "The person says woof!",
+        fur = "The person puts on a fur coat."
+    )
+
+class Dog(AnimalActions):
+    strings = dict(
+        quack = "The dog cannot quack.",
+        feathers = "The dog has no feathers.",
+        bark = "Arf!",
+        fur = "The dog has white fur with black spots."
+    )
+
+def in_the_doghouse(dog):
+    """ In the doghouse  method """
+    print(dog.bark())
+    print(dog.fur())
+
+def in_the_forest(duck):
+    """ In the forest method """
+    print(duck.quack())
+    print(duck.feathers())
+
+def main():
+    """ main method for the module """
+    donald = Duck()
+    john = Person()
+    fido = Dog()
+
+    print("- In the forest:")
+    for obj in (donald, john, fido):
+        in_the_forest(obj)
+
+    print("- In the doghouse:")
+    for obj in (donald, john, fido):
+        in_the_doghouse(obj)
+if __name__ == "__main__":
+    main()
